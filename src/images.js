7bf7aea3bb19aeef036ed192bedd72182bfddf16
Inkom007 <inkom0703@gmail.com>
2024-07-12 22:59:11 +0100

    update form

--- src/images.js
+++ src/images.js
@@ -1,8 +1,15 @@
 import image1 from "./assets/image1.jpg";
 import image2 from "./assets/image2.jpg";
 import image3 from "./assets/image3.jpg";
-import image4 from "./assets/image3.jpg";
+import image4 from "./assets/image4.jpg";
 import image5 from "./assets/image5.jpg";
+import image6 from "./assets/image6.jpg";
+import image7 from "./assets/image7.jpg";
+import image8 from "./assets/image8.jpg";
+import image9 from "./assets/image9.jpg";
+import image10 from "./assets/image10.jpg";
+import image11 from "./assets/image11.jpg";
+import image12 from "./assets/image12.jpg";
 
 const images = [
   { src: image1, alt: "image 1" },
@@ -10,6 +17,13 @@ const images = [
   { src: image3, alt: "image 3" },
   { src: image4, alt: "image 4" },
   { src: image5, alt: "image 5" },
+  { src: image6, alt: "image 6" },
+  { src: image7, alt: "image 7" },
+  { src: image8, alt: "image 8" },
+  { src: image9, alt: "image 9" },
+  { src: image10, alt: "image 10" },
+  { src: image11, alt: "image 11" },
+  { src: image12, alt: "image 12" },
 ];
 
 export default images;
